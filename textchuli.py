a92564329a9e7f43aa735926cd0b8933ed11fe62
yxken <32864430+yxken@users.noreply.github.com>
2018-01-23 14:36:01 +0800

    Update textchuli.py

--- textchuli.py
+++ textchuli.py
@@ -1,8 +1,8 @@
 import sys,getopt
 
 opts,args = getopt.getopt(sys.argv[1:],"hi:o:")
-input_file=""
-output_file=""
+input_file=""#输入文件(文件名)
+output_file=""#输出文件(文件名)
 for op,value in opts:
     if op == "-i":
        input_file = value
